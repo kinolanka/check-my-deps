18f6317ead68678043c7793adaab9576e41ccded
Volodymyr <kinolanka@gmail.com>
2024-11-19 18:34:26 +0200

    New tests

---
+++ src/utils/helpers/get-package-info.test.ts
@@ -0,0 +1,37 @@
+import getPackageInfo from './get-package-info';
+import fs from 'fs-extra';
+import path from 'path';
+
+jest.mock('fs-extra');
+jest.mock('path');
+
+describe('getPackageInfo', () => {
+  const mockPackageJson = {
+    name: 'test-package',
+    version: '1.0.0',
+  };
+
+  beforeEach(() => {
+    (path.join as jest.Mock).mockReturnValue('package.json');
+    (fs.readJSONSync as jest.Mock).mockReturnValue(mockPackageJson);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return the package.json content', () => {
+    const result = getPackageInfo();
+    expect(result).toEqual(mockPackageJson);
+  });
+
+  it('should call path.join with correct arguments', () => {
+    getPackageInfo();
+    expect(path.join).toHaveBeenCalledWith('package.json');
+  });
+
+  it('should call fs.readJSONSync with correct path', () => {
+    getPackageInfo();
+    expect(fs.readJSONSync).toHaveBeenCalledWith('package.json');
+  });
+});
