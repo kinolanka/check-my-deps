4e38bd641ae5b665f50d9eacf71bdabdbba0ef22
Volodymyr <kinolanka@gmail.com>
2025-03-07 00:40:43 +0200

    Silent parameter

--- src/services/service-ctx.ts
+++ src/services/service-ctx.ts
@@ -4,18 +4,21 @@ export type ServiceCtxType = {
   cwd: string;
   outputService: OutputService;
   outputDir?: string;
+  silent?: boolean;
 };
 
 class ServiceCtx {
   private _cwd: string;
   private _outputService: ServiceCtxType['outputService'];
   private _outputDir: string;
+  private _silent: boolean;
 
-  constructor({ cwd, outputService, outputDir }: ServiceCtxType) {
+  constructor({ cwd, outputService, outputDir, silent = false }: ServiceCtxType) {
     this._cwd = cwd;
     this._outputService = outputService;
     // Ensure outputDir is always a string by using cwd as fallback
     this._outputDir = outputDir || cwd;
+    this._silent = silent;
   }
 
   get cwd(): string {
@@ -28,6 +31,10 @@ class ServiceCtx {
 
   get outputDir(): string {
     return this._outputDir;
+  }
+
+  get silent(): boolean {
+    return this._silent;
   }
 }
 
